40507ce7f32142b31e6d328907b7f8f46fb7c44c
alvarohc777 <aherrada@uninorte.edu.co>
2023-05-09 09:45:49 -0500

    Update file only to use ref() instead of reactive()

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,6 +1,5 @@
-
 <template>
-  <q-page padding>
+  <q-page>
     <div
       class="col-md-6 col-xs-12 window-width row justify-evenly items-center"
     >
@@ -41,152 +40,41 @@
         <apex-donut :clientes="clientesFiltrado" />
       </q-card>
     </div>
-    <p>{{ clientes }}</p>
   </q-page>
 </template>
 
 <script setup>
-import {
-  computed,
-  defineComponent,
-  onMounted,
-  reactive,
-  ref,
-  watch,
-  watchEffect,
-} from "vue";
-import { api } from "src/boot/axios.js";
-import apexDonut from "../components/ApexDonut.vue";
-let nombres = reactive([]);
-let nombre = ref(null);
-let clientes = reactive([]);
-const x = ref([]);
-let clientesFiltrado = reactive([]);
-
-const getInfo = () => {
+import { onMounted, ref, reactive, watchEffect } from "vue";
+import ApexDonut from "src/components/ApexDonut.vue";
+import { api } from "../../src/boot/axios";
+const clientes = ref([]);
+const nombres = ref([]);
+const nombre = ref(null);
+const clientesFiltrado = ref([]);
+onMounted(() => {
   api
     .get("clientes")
     .then((res) => {
-      // res.json();
-      res.data.forEach((element) => {
-        clientes.push(element);
+      clientes.value = res.data;
+      clientes.value.forEach((cliente) => {
+        if (nombres.value.indexOf(cliente.nombre) == -1) {
+          nombres.value.push(cliente.nombre);
+        }
       });
-      //   clientes = res.data;
-      x.value = res.data;
-      console.log(clientes);
-      prueba();
     })
     .catch((err) => console.log(err.message));
-};
+});
 
-// const clientes = reactive([
-//   {
-//     nombre: "Luis Jimenez",
-//     nSolicitud: "12344",
-//     monto: "5000000",
-//     plazo: "3 meses",
-//     pago: "1800000",
-//     fechaCorte: "15/02/2023",
-//     documento: "",
-//     calificacion: "5",
-//   },
-//   {
-//     nombre: "Maria Aleja",
-//     nSolicitud: "12345",
-//     monto: "20000000",
-//     plazo: "1 año",
-//     pago: "3000000",
-//     fechaCorte: "30/04/2023",
-//     documento: "",
-//     calificacion: "4",
-//   },
-//   {
-//     nombre: "Luis Jimenez",
-//     nSolicitud: "12346",
-//     monto: "100000000",
-//     plazo: "1 año",
-//     pago: "2000000",
-//     fechaCorte: "30/03/2023",
-//     documento: "",
-//     calificacion: "3",
-//   },
-//   {
-//     nombre: "Luz Padilla",
-//     nSolicitud: "12347",
-//     monto: "10000000",
-//     plazo: "6 meses",
-//     pago: "900000",
-//     fechaCorte: "15/04/2023",
-//     documento: "",
-//     calificacion: "1",
-//   },
-//   {
-//     nombre: "Alvaro Herrada",
-//     nSolicitud: "12348",
-//     monto: "8000000",
-//     plazo: "10 meses",
-//     pago: "800000",
-//     fechaCorte: "15/04/2022",
-//     documento: "",
-//     calificacion: "5",
-//   },
-
-//   {
-//     nombre: "Alvaro Herrada",
-//     nSolicitud: "12349",
-//     monto: "1000000",
-//     plazo: "10 meses",
-//     pago: "100000",
-//     fechaCorte: "27/04/2022",
-//     documento: "",
-//     calificacion: "5",
-//   },
-//   {
-//     nombre: "Alvaro Herrada",
-//     nSolicitud: "12350",
-//     monto: "900000",
-//     plazo: "9 meses",
-//     pago: "100000",
-//     fechaCorte: "1/05/2022",
-//     documento: "",
-//     calificacion: "5",
-//   },
-// ]);
-
-let prueba = () => {
-  clientes.forEach((cliente) => {
-    if (nombres.indexOf(cliente.nombre) == -1) {
-      nombres.push(cliente.nombre);
-    }
-  });
-  //nombres = [...new Set(nombres)];
-};
-// prueba();
-
-// let nombresSet = computed(() => {
-//   let nombres = [];
-//   clientes.forEach((cliente) => {
-//     nombres.push(cliente.nombre);
-//   });
-//   nombres = [...new Set(nombres)];
-//   return nombres;
-// });
-watchEffect(async () => {
+watchEffect(() => {
   if (nombre.value) {
-    clientesFiltrado = await clientes.filter(
+    clientesFiltrado.value = clientes.value.filter(
       (cliente) => cliente.nombre === nombre.value
     );
   } else {
-    clientesFiltrado = await clientes;
+    clientesFiltrado.value = clientes.value;
   }
 });
-// let clientesFiltrado = computed(() => {
-//   if (nombre.value) {
-//     return clientes.filter((cliente) => cliente.nombre === nombre.value);
-//   } else {
-//     return clientes;
-//   }
-// });
+
 const columns = reactive([
   {
     name: "nombre",
@@ -250,16 +138,7 @@ const columns = reactive([
     sortable: true,
   },
 ]);
-
-watch(nombre, (newValue, oldValue) => {
-  console.log(nombre.value);
-});
-
-onMounted(() => {
-  getInfo();
-});
 </script>
-
 
 <style>
 .my-card {
