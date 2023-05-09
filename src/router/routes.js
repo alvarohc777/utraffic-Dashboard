32ba22e96ae46951b52dbaa253e616b2d6945b72
alvarohc777 <aherrada@uninorte.edu.co>
2023-05-09 09:47:16 -0500

    Remove Endpoints of incorrect Dashboard pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ import IndexLayout from "layouts/MainLayout";
 import IndexPage from "pages/IndexPage";
 import ErrorPage from "pages/ErrorNotFound.vue";
 import DashboardPage from "pages/DashboardPage";
-import DashboardLocal from "pages/DashboardLocal";
+
 const routes = [
   {
     path: "/",
@@ -10,7 +10,6 @@ const routes = [
     children: [
       { path: "", component: IndexPage },
       { path: "dashboard", component: DashboardPage },
-      { path: "dashboardLocal", component: DashboardLocal },
     ],
   },
 
