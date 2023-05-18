f6d656081b6b8e0815de242cabaa07bf6a4dee9d
alvarohc777 <aherrada@uninorte.edu.co>
2023-05-18 12:09:30 -0500

    Create utils scripts for reusability

---
+++ src/scripts/utils.js
@@ -0,0 +1,27 @@
+const progressCalculator = (planPago) => {
+  let pagos = 0;
+  let numeroCuotas = planPago.length;
+  planPago.forEach((cuota) => {
+    pagos += cuota.estado === "pagado" ? 1 : 0;
+    pagos += cuota.estado === "pagadoMora" ? 1 : 0;
+  });
+  return pagos / numeroCuotas;
+};
+const scoreCalculator = (planPago) => {
+  let cuotasEnMora = 0;
+  let numeroCuotasGeneradas = 0;
+  planPago.forEach((cuota) => {
+    // Total cuotas en mora / pagadas en mora
+    cuotasEnMora += cuota.estado === "mora" ? 1 : 0;
+    cuotasEnMora += cuota.estado === "pagadoMora" ? 1 : 0;
+    // Total cuotas generadas
+    numeroCuotasGeneradas += cuota.estado === "pagadoMora" ? 1 : 0;
+    numeroCuotasGeneradas += cuota.estado === "pagado" ? 1 : 0;
+    numeroCuotasGeneradas += cuota.estado === "mora" ? 1 : 0;
+  });
+  return Math.floor(
+    ((numeroCuotasGeneradas - cuotasEnMora) / numeroCuotasGeneradas) * 5
+  );
+};
+
+export { progressCalculator, scoreCalculator };
