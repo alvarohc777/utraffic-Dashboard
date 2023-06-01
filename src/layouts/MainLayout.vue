e73996912bd1c6c73dc364cdf15e1333c07e0e00
alvarohc777 <aherrada@uninorte.edu.co>
2023-06-01 16:26:14 -0500

    Change layout router-links

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,22 +7,15 @@
         </router-link>
 
 
-        <router-link :to="{ name: 'cliente' }">
-          <q-btn flat rounded color="dark" label="Cliente Stats"></q-btn>
-        </router-link>
+
         <router-link :to="{ name: 'pruebas' }">
           <q-btn flat rounded color="dark" label="Pruebas"></q-btn>
         </router-link>
         <router-link :to="{ name: 'credits' }">
-          <q-btn flat rounded color="dark" label="Creditos"></q-btn>
-        </router-link>
-        <router-link :to="{ name: 'asesor', params: { id: 10 } }">
-          <q-btn flat rounded color="dark" label="Blog"></q-btn>
+          <q-btn unelevated rounded color="purple-10" label="Creditos"></q-btn>
         </router-link>
 
-        <q-btn unelevated rounded color="purple-10">
-          <div>¿Ya Eres Cliente?</div>
-        </q-btn>
+
       </q-toolbar>
     </q-header>
 
